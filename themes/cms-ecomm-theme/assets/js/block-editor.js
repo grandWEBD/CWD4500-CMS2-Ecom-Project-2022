a31c92caefa737857288b7c61e6e24b80d1d6544
Heather <heather.grandovec@dcmail.ca>
2022-02-07 13:02:22 -0500

    added block-editor js and php files

---
+++ themes/cms-ecomm-theme/assets/js/block-editor.js
@@ -0,0 +1,10 @@
+console.log('Block Editor');
+
+wp.blocks.registerBlockStyle( 'core/quote', {
+    name: 'cms-ecomm-theme-quote',
+    label: 'my theme Quote',
+} );
+
+wp.domReady( function () {
+    wp.blocks.unregisterBlockStyle( 'core/quote', 'large' );
+} );
--- themes/cms-ecomm-theme/functions.php
+++ themes/cms-ecomm-theme/functions.php
@@ -150,3 +150,7 @@ require get_template_directory() . '/inc/template-functions.php';
 require get_template_directory() . '/inc/customizer.php';
 
 
+/**
+ * Block Editor additions.
+ */
+require get_template_directory() . '/inc/block-editor.php';
